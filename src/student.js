af132d5c643c6bc494fb3630a0213d823ac02ccd
vikasgist8056 <vikas.gist8056@gmail.com>
2026-09-23 08:23:27 +0530

    first commit

---
+++ src/student.js
@@ -0,0 +1,17 @@
+export const students = [
+    {
+        id: 1,
+        name: "Rahul",
+        marks: 87
+    },
+    {
+        id: 2,
+        name: "Aman",
+        marks: 92
+    },
+    {
+        id: 3,
+        name: "Neha",
+        marks: 35
+    }
+];
